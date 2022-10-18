9b0db5a4355fc07996c0ae6515cfd1937364b744
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-10-18 17:45:33 +0330

    Create html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import getShows from './modules/getShows.js';
+// import getLikes from './modules/getLikes.js';
+import ShowLibrary from './modules/ShowLibrary.js';
+import './desktop.css';
 import './style.css';
+
+const showContainer = document.getElementById('showList');
+const result = getShows();
+result.then((value) => {
+  const showLib = new ShowLibrary(value.data);
+  showLib.displayShows(10, showContainer);
+});
+// .catch((err) => {
+//   console.log(err);
+// });
