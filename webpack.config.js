fcb171b35d44d1e4111aac75c0897ccb889215c0
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-10-18 17:45:13 +0330

    Update project Config

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,20 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
