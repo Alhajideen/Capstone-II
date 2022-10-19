bc7bcad19163c2fe0a421249c8e756e575666cf0
Alhajideen <salifunurudeen834@gmail.com>
2022-10-19 09:14:28 -0600

    write test for the comment count function

---
+++ src/__test__/comment.test.js
@@ -0,0 +1,52 @@
+import { cmtCount } from '../modules/commentpop';
+
+describe('Total comments on a post', () => {
+  test('Count four comments', () => {
+    //Assign
+    const commentArr = [
+      {
+        username: 'John',
+        creation_date: '2022-19-10',
+        comment: 'Great movie',
+      },
+      {
+        username: 'Jane',
+        creation_date: '2022-19-10',
+        comment: 'I like this movie',
+      },
+      {
+        username: 'John',
+        creation_date: '2022-19-10',
+        comment: 'Great movie',
+      },
+      {
+        username: 'John',
+        creation_date: '2022-19-10',
+        comment: 'I will watch this movie',
+      },
+    ];
+
+    // Act
+    const totalComments = cmtCount(commentArr);
+
+    //Assert
+    expect(totalComments).toBe(4);
+  });
+
+  test('Count one comment', () => {
+    //Assign
+    const commentArr = [
+      {
+        username: 'John',
+        creation_date: '2022-19-10',
+        comment: 'Great movie',
+      }
+    ];
+
+    // Act
+    const totalComments = cmtCount(commentArr);
+
+    //Assert
+    expect(totalComments).toBe(1);
+  });
+});
